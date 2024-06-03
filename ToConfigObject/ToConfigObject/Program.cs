6e2a86b9e485dd2ba06a51a04ff8cfbcd6dc4017
Ekaterina Schneider <kat1906200152@yandex.ru>
2024-06-03 21:37:34 +0500

    Создала скелет, думаю над фабричным методом

---
+++ ToConfigObject/ToConfigObject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ToConfiguration
+{
+    // Product
+    public class Configuration
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    // Concrete products
+    public class ConfigurationFromXML : Configuration { }
+    public class ConfigurationFromCSV : Configuration { }
+
+
+    // Creator
+    public abstract class Configurator
+    {
+        public abstract Configuration GetConfiguration();
+    }
+    // Concrete creators
+    public class ConfiguratorFromXML : Configurator
+    {
+        public override Configuration GetConfiguration() 
+        { 
+            return new ConfigurationFromXML();
+        }
+    }
+    public class ConfiguratorFromCSV : Configurator
+    {
+        public override Configuration GetConfiguration()
+        {
+            return new ConfigurationFromCSV();
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args) 
+        { 
+            
+        }
+    }
+
+}
